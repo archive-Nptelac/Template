51810d5360cc5e6b88d94cc943a24b5209c2a990
KBS05 <85780168+KBS05@users.noreply.github.com>
2021-08-16 11:25:56 +0530

    Update DSU.cpp

--- Algorithms/DSU.cpp
+++ Algorithms/DSU.cpp
@@ -1,20 +1,25 @@
-int par[MAX];
-int sz[MAX];
-void init(){
-    for(int i=0;i<MAX;i++){
-        par[i]=i;
-        sz[i]=1;
+class DSU{
+    vector<int>par;
+    vector<int>sz;
+public:
+    DSU(int n){
+        par.resize(n+5);
+        sz.resize(n+5,1);
+        iota(par.begin(),par.end(),0);
     }
-}
-int parent(int x){
-    if(par[x]==x) return x;
-    par[x]=parent(par[x]);
-    return par[x];
-}
-void unionxy(int x,int y){
-    int px=parent(x);
-    int py=parent(y);
-    if(px==py) return;
-    par[py]=px;
-    sz[px]+=sz[py];
-}
+    int parent(int x){
+        if(par[x]==x) return x;
+        return par[x]=parent(par[x]);
+    }
+    bool is_same(int x,int y){
+        return (parent(x)==parent(y));
+    }
+    void unionxy(int x,int y){
+        int px=parent(x);
+        int py=parent(y);
+        if(px==py) return;
+        if(sz[px]<sz[py])   swap(px,py);
+        par[py]=px;
+        sz[px]+=sz[py];
+    }
+};
